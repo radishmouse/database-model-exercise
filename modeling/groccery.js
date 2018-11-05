f53bf18dee933f30bc12754a861cffd530ea08e6
mkdir-helen <experiment.helen@gmail.com>
2018-11-05 16:10:51 -0500

    Added a groccery shopping list with node dependencies

---
+++ modeling/groccery.js
@@ -0,0 +1,74 @@
+const pgp = require('pg-promise')();
+const db = pgp ({
+    host: 'localhost',
+    port: 5432,
+    database: 'groccery-list'
+});
+
+//CREATE
+function add(name){
+    return db.result(`insert into gList(name, quantity, completed)
+        values
+            ($1, $2, $3)
+        returning id
+        `, [name, quantity, completed])
+}
+
+//RETRIEVE
+function getAll(){
+    return db.any('select * from gList');
+}
+
+function getById(id){
+    return db.one(`select * from gList where id=$1`, [id])
+        .catch(err => {
+            return {
+                name: 'No todo found.'
+            };
+        })
+}
+
+//UPDATE
+
+function updateList(id, didComplete){
+    return db.result(`update gList
+        set completed=$2
+    where id=$1`, [id, didComplete])
+}
+function markCompleted(id){
+    return updateList(id, true);
+}
+
+function markPending(id){
+    return updateList(id, false);
+}
+
+function updateName(id, name){
+    return db.result(`update gList
+        set name=$2
+    where id=$1`, [id, name]);
+}
+
+function updateQuantity(name, quantity){
+    return db.result(`update gList
+        set quantity=$1
+    where id=(select * from gList where name ilike $2)`, [quantity, name]);
+}
+
+//DELETE
+
+function deleteByName(name){
+    return db.result(`delete from todos where id=(select * from gList where name ilike $1)`);
+}
+
+
+module.exports = {
+    add,
+    getAll,
+    getById,
+    markCompleted,
+    markPending,
+    updateName,
+    updateQuantity,
+    deleteByName
+};
---
+++ seed.sql
@@ -0,0 +1,11 @@
+insert into gList
+    (name, quantity, completed)
+VALUES
+    ('tomatoes', 8, false),    
+    ('milk', 2, false),    
+    ('onions', 3, false),    
+    ('potatoes', 4, false),    
+    ('water gallon', 10, false),    
+    ('durian', 1, false),    
+    ('lettuce', 1, false),    
+    ('vodka', 20, false)    
