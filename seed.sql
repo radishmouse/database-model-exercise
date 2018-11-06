87d3b3c1248dd2a31906dde7f6659f5f343c3e2c
Amelia Schulz <you@example.com>
2018-11-05 22:18:13 -0500

    sets up pg promise and dummy data

---
+++ seed.sql
@@ -0,0 +1,9 @@
+insert into groceryitems
+    (name, quantity)
+values
+    ('la croix', 20),
+    ('almond milk', 1),
+    ('frozen pizza', 2),
+    ('discounted halloween candy', 3),
+    ('honeycrisp apples', 6),
+    ('wheaties', 1)
