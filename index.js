06fac3506cacaa485e0f43fc32cee207498a65b5
Edward Whelan <vroxing@gmail.com>
2018-11-05 19:14:45 -0500

    add retrieve/add functions for CRUD

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 const Blog = require('./models/Blog');
 
-Blog.showAll();
+Blog.showAllBlogs()
+  .then(result => {
+    console.log(result);
+  })
+
+Blog.showAllComments()
+  .then(result => {
+    console.log(result);
+  })
+
+// Blog.addABlog('How i went to the store', 'alsdlkasjdkjads', 2);
+// Blog.addAUser('Edward Whelan');
+
--- models/Blog.js
+++ models/Blog.js
@@ -1,7 +1,47 @@
-function showAll() {
-  console.log('it works');
+const pgp = require('pg-promise')();
+
+const db = pgp({
+  host: 'localhost',
+  port: 5432,
+  database: 'blogblogblog-db'
+});
+
+// CREATE
+// make a blog post
+function addABlog(title, post, author_id) {
+  return db.one(`insert into blogs(title, post, author_id) values ($1, $2, $3)
+  returning id`, [title, post, author_id])
+}
+//make a user
+function addAUser(name) {
+  return db.one(`insert into users(name) values ($1)
+  returning id`, [name])
+}
+//make a comment
+
+//RETRIEVE
+//show all blogs/users
+function showAllBlogs() {
+  return db.any('select * from blogs')
 }
 
+function showAllUsers() {
+  return db.any('select * from users')
+}
+
+function showAllComments() {
+  return db.any('select * from comments')
+}
+// Show blog with name
+//UPDATE
+
+//DELETE
+
+//functions to export go below
 module.exports = {
-  showAll,
+  showAllBlogs,
+  showAllUsers,
+  showAllComments,
+  addABlog,
+  addAUser,
 }
