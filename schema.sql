47365fd72160c855597afcdc6e4255fd58c4bcda
Alex Rogers <alex.rogers823@gmail.com>
2018-11-06 09:00:01 -0500

    completed grocery list one

---
+++ schema.sql
@@ -0,0 +1,7 @@
+-- groceries
+
+create table groceryitems (
+  id serial primary key,
+  name varchar(100),
+  quantity integer
+);
---
+++ seed.sql
@@ -0,0 +1,9 @@
+insert into groceryitems
+  (name, quantity)
+values
+  ('Honey Buns', 20),
+  ('Twinkies', 10),
+  ('Ding Dongs', 5),
+  ('Ho-Hos', 6),
+  ('Swiss Cake Rolls', 15),
+  ('Actual Food', 1)
