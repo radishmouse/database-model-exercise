5ac773d030c830faffd9b3ba3c21ed23c8764869
Ian Hundere <ianfundere@gmail.com>
2018-11-06 09:31:11 -0500

    adds commands to index.js

---
+++ index.js
@@ -0,0 +1,23 @@
+Grocery.add("dog food", 1)
+  .catch(err => {
+    console.log(err);
+  })
+  .then(result => {
+    console.log(result);
+  });
+
+Grocery.getAll().then(result => {
+  console.log(result);
+});
+
+Grocery.getById(4).then(result => {
+  console.log(result);
+});
+
+Grocery.updateName(6, "Tortillas").then(result => {
+  console.log(result);
+});
+
+Grocery.deleteById(6).then(result => {
+  console.log(result.rowCount);
+});
